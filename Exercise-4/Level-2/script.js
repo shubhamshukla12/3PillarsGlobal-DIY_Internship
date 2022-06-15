f83236e451cf32545fd774ea1b1d41af1c5358eb
Shubham Shukla <67850120+shubhamshukla12@users.noreply.github.com>
2022-06-15 22:43:18 +0530

    exercise-4 l2 update

--- Exercise-4/Level-2/script.js
+++ Exercise-4/Level-2/script.js
@@ -50,6 +50,50 @@ const users = {
     }
 }
 
-document.write(
-    users.skills.cou
-)  
+let max=0,l=0,ans,loggedIn=0,points=0;
+const mern = [];
+for(const key of Object.keys(users))
+{
+    l = users[key].skills.length;
+    if(l>max)
+    {
+        max=l;
+        ans=key;
+    }
+    if(users[key].isLoggedIn)
+        loggedIn++;
+    if(users[key].points >= 50)
+        points++;
+    if((users[key].skills.includes('MongoDB')) && (users[key].skills.includes('Node')) &&
+    (users[key].skills.includes('React')) && (users[key].skills.includes('Express')))
+        mern.push(key);
+}
+console.log("User with most skills is "+ans);
+console.log("Number of logged in users "+loggedIn);
+console.log("Number of users with greater than or equal to 50 points "+points);
+console.log("Mern stack developers: "+mern);
+
+document.write("User with most skills is "+ans);
+document.write(" \nNumber of logged in users "+loggedIn);
+document.write(" \nNumber of users with greater than or equal to 50 points "+points);
+document.write(" \nMern stack developers: "+mern);
+
+
+// creating a new key which is an object
+let Rohan = {
+    email: 'rohan@rohan.com',
+    skills: ['HTML', 'CSS', 'JavaScript'],
+    age: 20,
+    isLoggedIn: false,
+    points: 30
+}
+// copying users object to new object without modifying the original users object
+let copyUsers = Object.assign({}, users);
+// setting new key in copied object 
+copyUsers = {...copyUsers, Rohan};
+console.log(copyUsers);
+
+console.log(Object.keys(users));
+
+console.log(Object.values(users));
+ 
